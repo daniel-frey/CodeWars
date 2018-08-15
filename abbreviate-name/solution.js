12db46b9f84c196e2ffb7d331b89566cbbe05654
Daniel Frey <dann.frey@me.com>
2018-08-15 08:36:54 -0700

    codewars three

--- abbreviate-name/solution.js
+++ abbreviate-name/solution.js
@@ -1,5 +1,5 @@
 function abbrevName(name){
 
   let nameArray = name.split(' ');
-  return (nameArray[0][0] + '.' + nameArray[1][0]).toUpperCase();
-}
+  return (`${nameArray[0][0].toUpperCase()}.${nameArray[1][0].toUpperCase()}`);
+  }
